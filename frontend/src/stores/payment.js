c7274c9a3240f9d3d0b3edb545a5e4afd641c8c4
cen1c <sbfactual@gmail.com>
2024-11-21 19:15:17 +0530

    1st

---
+++ frontend/src/stores/payment.js
@@ -0,0 +1,9 @@
+import {defineStore} from "pinia"
+import {ref} from 'vue'
+import axios from 'axios'
+import { useRouter } from "vue-router"
+
+
+export const usePaymentStore = defineStore('payment', ()=>{
+    
+})
